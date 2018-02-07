476f661f24d593ddc9f44b343518ad823d56cbe0
igriffin415 <fireice16@yahoo.com>
2018-02-07 15:23:06 -0500

    mm

--- bot.js
+++ bot.js
@@ -11,8 +11,8 @@ client.on('ready', () => {
 client.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
