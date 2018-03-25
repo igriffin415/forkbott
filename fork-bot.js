f5d992148c20223585b9b964ba08ffd7c1e728eb
igriffin415 <fireice16@yahoo.com>
2018-03-24 23:53:36 -0400

    hm

--- fork-bot.js
+++ fork-bot.js
@@ -1,16 +1,35 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+// var forks = require("./forks.js");
+// var theForks = forks.largeArrayFunction();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+  //  if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            case 'fork':
+              //var fork = theForks[ (Math.floor(Math.random() * theForks.length)) ];
+              message.channel.send("wow");
+              break;
+            case 'iloveisabelle':
+              message.channel.send("isabelle is your god now and also the best coder");
+              break;
+            case 'ilovejoss':
+              message.channel.send("joss is best mom");
+            break;
+            // Just add any case commands if you want to..
+         }
+    // }
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
