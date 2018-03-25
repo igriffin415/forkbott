bb8666dfdcc36ffc0bf23127e8f8baae859e06a4
igriffin415 <fireice16@yahoo.com>
2018-03-24 23:54:34 -0400

    please jesus

--- fork-bot.js
+++ fork-bot.js
@@ -1,23 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// var forks = require("./forks.js");
-// var theForks = forks.largeArrayFunction();
+ var forks = require("./forks.js");
+ var theForks = forks.largeArrayFunction();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-  //  if (message.content.substring(0, 1) == '!') {
+    if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
         switch(cmd) {
             case 'fork':
-              //var fork = theForks[ (Math.floor(Math.random() * theForks.length)) ];
-              message.channel.send("wow");
+              var fork = theForks[ (Math.floor(Math.random() * theForks.length)) ];
+              message.channel.send(fork);
               break;
             case 'iloveisabelle':
               message.channel.send("isabelle is your god now and also the best coder");
@@ -27,7 +27,7 @@ client.on('message', message => {
             break;
             // Just add any case commands if you want to..
          }
-    // }
+     }
 });
 
 
